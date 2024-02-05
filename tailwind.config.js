663207640ff67e7810bdf797fbe001d7c78e5677
Rukundo Kevin <kevinrukundo1@gmail.com>
2024-02-05 15:13:26 +0200

    Add home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#007bff',
+        btnLight: '#0039a6',
+        btnDark: '#FF4D4D',
+      }
+    },
   },
   plugins: [],
 }
